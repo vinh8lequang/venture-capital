65b74dff3325dc3a58ec9ebe053f0b742ba0f252
Vinh Le Quang <vinh8lequang@gmail.com>
2023-06-07 15:48:28 +0200

    nav menu and project id page

--- components/NavMenu.vue
+++ components/NavMenu.vue
@@ -1,11 +1,26 @@
 <template>
   <v-card>
-    <v-tabs v-model="tab" color="#203C8A" align-tabs="center">
-      <NuxtLink to="/"><v-tab>Home</v-tab></NuxtLink>
-      <NuxtLink to="/categories"><v-tab>Categories</v-tab></NuxtLink>
-      <NuxtLink to="/projects"><v-tab>Projects</v-tab></NuxtLink>
-      <NuxtLink to="/team"><v-tab>Team</v-tab></NuxtLink>
-    </v-tabs>
+    <v-toolbar color="#fff">
+      <!-- Navigation Tabs -->
+      <v-tabs v-model="tab" color="#203C8A" align-tabs="center" class="ml-4">
+        <NuxtLink to="/"><v-tab>Home</v-tab></NuxtLink>
+        <NuxtLink to="/categories"><v-tab>Categories</v-tab></NuxtLink>
+        <NuxtLink to="/projects"><v-tab>Projects</v-tab></NuxtLink>
+        <NuxtLink to="/team"><v-tab>Team</v-tab></NuxtLink>
+      </v-tabs>
+
+      <!-- Apply and FAQ Buttons -->
+      <div class="flex-grow-1"></div>
+      <!-- Create a flexible space to push buttons to the right -->
+      <div>
+        <NuxtLink to="/faq"
+          ><v-btn color="#4e76e6" class="ml-2">FAQ</v-btn></NuxtLink
+        >
+        <NuxtLink to="/apply"
+          ><v-btn color="#4e76e6" class="ml-2">Apply</v-btn></NuxtLink
+        >
+      </div>
+    </v-toolbar>
   </v-card>
 </template>
 
