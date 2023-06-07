b77ef04ae293e1b84c06f74b8f80f338688e842f
Vinh Le Quang <vinh8lequang@gmail.com>
2023-06-07 12:09:36 +0200

    little update on people db

--- components/Person_card.vue
+++ components/Person_card.vue
@@ -1,49 +1,41 @@
 <template>
-    <v-card
-      class="mx-auto"
-      max-width="344"
-      color="#f0e9e9"
-    >
-      <v-img :src="person.image" height="200px" cover></v-img>
-  
-      <v-card-title>
-        {{person.name}}
-      </v-card-title>
-  
-      <v-card-subtitle>
-        {{person.email}}
-      </v-card-subtitle>
-  
-      <v-card-actions>
-        <v-btn
-          color="orange-lighten-2"
-          variant="text"
-        >
-          See more
-        </v-btn>
-  
-        <v-spacer></v-spacer>
-  
-        <v-btn
-          :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
-          @click="show = !show"
-        ></v-btn>
-      </v-card-actions>
-  
-      <v-expand-transition>
-        <div v-show="show">
-          <v-divider></v-divider>
-  
-          <v-card-text>
-            {{person.description}}
-          </v-card-text>
-        </div>
-      </v-expand-transition>
-    </v-card>
-  </template>
+  <v-card class="mx-auto" max-width="344" color="#f0e9e9">
+    <v-img :src="person.image" height="200px" cover></v-img>
+
+    <v-card-title>
+      {{ person.name }}
+      {{ person.lastname }}
+    </v-card-title>
+
+    <v-card-subtitle>
+      {{ person.email }}
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn color="orange-lighten-2" variant="text"> See more </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+        @click="show = !show"
+      ></v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          {{ person.expertise }}
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
 
 <script setup>
-  const { person } = defineProps(['person'])
+const { person } = defineProps(["person"]);
 </script>
 
 <script>
@@ -51,5 +43,5 @@ export default {
   data: () => ({
     show: false,
   }),
-}
+};
 </script>
